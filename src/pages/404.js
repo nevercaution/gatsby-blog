7765ac31cc3f94caf8ec86788300f18a66aac27c
teddy <nevercaution@gmail.com>
2019-10-07 17:19:44 +0900

    404 페이지 업데이트

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,9 +3,8 @@ import Layout from '../components/layout';
 
 const NotFoundPage = () => (
   <Layout>
-    <h1>NOT FOUND</h1>
-    <p>블로그 공사중으로 기존 포스트가 아직 업데이트되지 않았을 수 있습니다.</p>
-    <p>빠른 시일 내에 업데이트를 완료하겠습니다.</p>
+    <h1>404</h1>
+    <p>This is not the web page you are looking for.</p>
   </Layout>
 );
 
